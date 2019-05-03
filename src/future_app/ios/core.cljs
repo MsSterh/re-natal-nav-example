ceae20c1a35d6bbeb22722ea5757e7ecf75becad
Anastasiia Bakuta <ms.sterh@gmail.com>
2019-05-03 18:03:51 +0300

    Added react-navigation

--- src/future_app/ios/core.cljs
+++ src/future_app/ios/core.cljs
@@ -4,7 +4,13 @@
             [future-app.events]
             [future-app.subs]))
 
+;; Fix 'Go Back' navigation
+(js/require "react-native-gesture-handler")
+
 (def ReactNative (js/require "react-native"))
+(def ReactNavigation (js/require "react-navigation"))
+(def create-stack-navigator (.-createStackNavigator ReactNavigation))
+(def create-app-container (.-createAppContainer ReactNavigation))
 
 (def app-registry (.-AppRegistry ReactNative))
 (def text (r/adapt-react-class (.-Text ReactNative)))
@@ -17,16 +23,32 @@
 (defn alert [title]
       (.alert (.-Alert ReactNative) title))
 
-(defn app-root []
-  (let [greeting (subscribe [:get-greeting])]
-    (fn []
-      [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
-       [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} @greeting]
-       [image {:source logo-img
-               :style  {:width 80 :height 80 :margin-bottom 30}}]
-       [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
-                             :on-press #(alert "HELLO!")}
-        [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]])))
+(defn _firstScreen [props]
+  (fn [props]
+    [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
+     [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} "@greeting"]
+     [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
+                           :on-press (fn []
+                                        ;; which is equal to props.navigation.navigate("SecondScreen")
+                                       (.navigate (.-navigation (clj->js props)) "SecondScreen"))}
+      [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "next screen"]]]))
+
+(defn _secondScreen [props]
+  (fn [props]
+    [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
+      [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} "@greeting"]
+      [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
+                            :on-press (fn []
+                                        (.goBack (.-navigation (clj->js props))))}
+      [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "go back"]]]))
+
+(def AppNavigator
+  (create-stack-navigator
+   (clj->js {:FirstScreen  (r/reactify-component _firstScreen)
+             :SecondScreen (r/reactify-component _secondScreen)})
+   (clj->js {:initialRouteName "FirstScreen"})))
+
+(defn app-root [] [:> (create-app-container AppNavigator) {}])
 
 (defn init []
       (dispatch-sync [:initialize-db])
